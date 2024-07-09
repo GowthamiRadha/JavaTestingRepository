c102d4afbee420f4c8f455a77656f9d16a846554
Gowthami Radha Ananthaneni <37111979+GowthamiRadha@users.noreply.github.com>
2024-07-08 20:34:36 -0700

    Update Main.java

--- Testing/src/main/java/Main.java
+++ Testing/src/main/java/Main.java
@@ -1,10 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        int a = Integer.parseInt(args[0]);
-        int b = Integer.parseInt(args[1]);
-        System.out.println("Addition: " + ArithmeticOperations.add(a, b));
-        System.out.println("Addition: " + ArithmeticOperations.multiply(a, b));
-        System.out.println("Addition: " + ArithmeticOperations.divide(a, b));
-        System.out.println("Divide: "+ArithmeticOperations.divide(a, b));
+        int a = Integer.parseInt(args[1]);
+        int b = Integer.parseInt(args[2]);
+        int result = 0;
+
+        switch (args[0].toLowerCase()) {
+            case "add":
+                result = ArithmeticOperations.add(a, b);
+                break;
+            case "subtract":
+                result = ArithmeticOperations.subtract(a, b);
+                break;
+            case "multiply":
+                result = ArithmeticOperations.multiply(a, b);
+                break;
+            case "divide":
+                result = ArithmeticOperations.divide(a, b);
+                break;
+        }
+
+        System.out.println(result);
     }
 }
