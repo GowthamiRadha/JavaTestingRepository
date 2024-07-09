996df3a549c1613dac6721cc36e51e40a52dfce1
Gowthami Radha Ananthaneni <37111979+GowthamiRadha@users.noreply.github.com>
2024-07-08 21:12:11 -0700

    Update ArithmeticOperations.java

--- Testing/src/main/java/ArithmeticOperations.java
+++ Testing/src/main/java/ArithmeticOperations.java
@@ -5,6 +5,10 @@ public class ArithmeticOperations {
     }
 
     public static int subtract(int a, int b) {
+         if (b == 0) {
+            System.out.println("Exception");  // Print a specific message instead of throwing an exception
+            return Integer.MIN_VALUE;         // Return a special value indicating an error (could also be 0 or any other sentinel value)
+        }
         return a-b;
     }
 
