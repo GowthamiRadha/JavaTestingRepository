62dba0bf065df0a4d3af28e2185914fc9c45525c
Gowthami Radha Ananthaneni <37111979+GowthamiRadha@users.noreply.github.com>
2024-07-08 20:17:02 -0700

    Create ArithmeticOperations.java

---
+++ Testing/src/main/java/ArithmeticOperations.java
@@ -0,0 +1,21 @@
+public class ArithmeticOperations {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Divider cannot be zero.");
+        }
+        return a / b;
+    }
+}
