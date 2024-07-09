61d37c8e26140f23086d98e2a582603e1eee6c84
GowthamiRadha <ananthanenigowthami@gmail.com>
2024-07-08 20:58:22 -0700

    Added Arithmetic operations

--- Testing/src/main/java/ArithmeticOperations.java
+++ Testing/src/main/java/ArithmeticOperations.java
@@ -1,18 +1,18 @@
 public class ArithmeticOperations {
 
     public static int add(int a, int b) {
-        return 0;
+        return a+b;
     }
 
     public static int subtract(int a, int b) {
-        return 0;
+        return a-b;
     }
 
     public static int multiply(int a, int b) {
-        return 0;
+        return a*b;
     }
 
     public static int divide(int a, int b) {
-        return 0;
+        return a/b;
     }
 }
